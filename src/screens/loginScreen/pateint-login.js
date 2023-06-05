b9de7a65d913bde99ffe0d275aa6cb1ec08e0804
Aman kumar Patel <97835747+Amankumarpatel123@users.noreply.github.com>
2023-06-05 23:13:43 +0530

    Add files via upload

---
+++ src/screens/loginScreen/pateint-login.js
@@ -0,0 +1,162 @@
+import "./pateint-login.css";
+const PateintLogin = () => {
+  return (
+    <div className="pateint-login">
+      <div className="pateint-login-child" />
+      <div className="group-parent">
+        <div className="ranja-hospital-parent">
+          <div className="ranja-hospital">Ranja Hospital</div>
+          <img
+            className="standard-collection-12"
+            alt=""
+            src="/standard-collection-12.svg"
+          />
+        </div>
+        <div className="quick-links-parent">
+          <b className="quick-links">Quick Links</b>
+          <div className="home">Home</div>
+          <div className="about-us">About Us</div>
+          <div className="contact-us">Contact Us</div>
+          <div className="appointment">Appointment</div>
+          <div className="service">Service</div>
+        </div>
+        <div className="group-container">
+          <div className="group-wrapper">
+            <div className="group-wrapper">
+              <b className="quick-links">Contact</b>
+              <div className="div">000-000-000</div>
+              <div className="infoemailcom">info@email.com</div>
+              <div className="kalutara-south">Kalutara South</div>
+            </div>
+          </div>
+          <img className="vector-icon" alt="" src="/vector.svg" />
+          <img className="phone-alt-icon" alt="" src="/phonealt.svg" />
+          <img
+            className="location-arrow-icon"
+            alt=""
+            src="/locationarrow.svg"
+          />
+        </div>
+        <div className="group-frame">
+          <div className="group-wrapper">
+            <b className="quick-links">Hours</b>
+            <div className="monday">{`Monday:            `}</div>
+            <div className="div1">9:00 - 18:00</div>
+            <div className="div2">9:00 - 18:00</div>
+            <div className="div3">9:00 - 18:00</div>
+            <div className="div4">9:00 - 18:00</div>
+            <div className="div5">9:00 - 18:00</div>
+            <div className="thursday">Thursday:</div>
+            <div className="friday">Friday:</div>
+            <div className="tuesday">{`Tuesday:            `}</div>
+            <div className="wednesday">Wednesday:</div>
+          </div>
+        </div>
+        <img className="instance-child" alt="" src="/vector-1.svg" />
+      </div>
+      <div className="vector-parent">
+        <img className="vector-icon1" alt="" src="/vector1.svg" />
+        <div className="instance-wrapper">
+          <div className="group-div">
+            <div className="group-wrapper">
+              <div className="group-child" />
+              <div className="group-item" />
+              <b className="register">Register</b>
+            </div>
+          </div>
+        </div>
+        <div className="home-parent">
+          <b className="home1">Home</b>
+          <b className="appointment1">Appointment</b>
+          <b className="services">Services</b>
+          <b className="about-us1">About Us</b>
+          <b className="contact-us1">Contact Us</b>
+        </div>
+        <div className="medilab-hospital-parent">
+          <div className="medilab-hospital">MediLab Hospital</div>
+          <img
+            className="standard-collection-121"
+            alt=""
+            src="/standard-collection-121.svg"
+          />
+        </div>
+        <div className="we-help-people-to-get-appointm-parent">
+          <div className="we-help-people-container">
+            <span className="we-help-people-container1">
+              <p className="we-help-people">{`We help people to `}</p>
+              <p className="we-help-people">{`get appointment `}</p>
+              <p className="in-online">in online</p>
+            </span>
+          </div>
+          <div className="lorem-media-is">
+            Lorem Media is a full-service social media agency. We offer
+            businesses innovative solutions that deliver the right type of
+            audience to you in the most effective strategies possible. We strive
+            to develop a community around your business, polishing your
+            branding, and improving your public relations.
+          </div>
+        </div>
+      </div>
+      <div className="sign-in">Sign In</div>
+      <div className="please-login-to">Please Login To Continue</div>
+      <div className="lorem-ipsum-is-container">
+        <p className="lorem-ipsum-is">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+        <p className="in-online">
+          Lorem Ipsum has been the industry's standard dummy text ever since.
+        </p>
+      </div>
+      <div className="input-field">
+        <div className="input-field-child" />
+        <div className="first-name">Email</div>
+      </div>
+      <div className="input-field1">
+        <div className="input-field-child" />
+        <div className="first-name">Password</div>
+      </div>
+      <div className="btntour">
+        <div className="btntour-child" />
+        <b className="sign-in1">Sign in</b>
+      </div>
+      <div className="objectother-15">
+        <div className="objectother-03-parent">
+          <div className="objectother-03">
+            <img
+              className="objectother-03-icon"
+              alt=""
+              src="/objectother-03@2x.png"
+            />
+          </div>
+          <img
+            className="objectother-15-icon"
+            alt=""
+            src="/objectother-15@2x.png"
+          />
+        </div>
+      </div>
+      <img
+        className="employee-providing-information"
+        alt=""
+        src="/employee-providing-information-to-his-superior--640x427-1@2x.png"
+      />
+      <img
+        className="web-developer-doing-coding-6"
+        alt=""
+        src="/web-developer-doing-coding--640x533-1@2x.png"
+      />
+      <div className="rectangle-group">
+        <div className="instance-item" />
+        <div className="instance-inner" />
+        <div className="rectangle-div" />
+        <b className="patient">Patient</b>
+        <b className="doctor">Doctor</b>
+        <b className="admin">Admin</b>
+      </div>
+      <img className="vector-icon2" alt="" src="/vector2.svg" />
+    </div>
+  );
+};
+
+export default PateintLogin;
